029915a5388291f6ae533ffc36c5aefdd31fce5e
Dorian-T <dorian.tonnis@hotmail.fr>
2023-03-07 09:16:24 +0100

    Classe Entity

--- src/core/Entity.cpp
+++ src/core/Entity.cpp
@@ -1,34 +1,41 @@
-#include <iostream>
-
 #include "Vector2D.h"
 #include "Entity.h"
+#include <iostream>
+#include <cassert>
+using namespace std;
 
-Entity::Entity()
-{
-    position = Vector2D(0,0);
+Entity::Entity() {
+    position = Vector2D(0, 0);
     h = 0;
     w = 0;
 }
 
-Entity::Entity(Vector2D Eposition , unsigned int Eh, unsigned int Ew)
-{
+Entity::Entity(Vector2D Eposition , unsigned int Eh, unsigned int Ew) {
     position = Eposition;
     h = Eh;
-    Ew = w;
+    w = Ew;
 }
 
-unsigned int Entity::GetH()
-{
+unsigned int Entity::getH() {
     return h;
 }
 
-unsigned int Entity::GetW()
-{
+unsigned int Entity::getW() {
     return w;
 }
 
-
-Vector2D Entity::GetPosition()
-{
+Vector2D Entity::getPosition() {
     return position;
+}
+
+void Entity::regressionTest() {
+    cout << "Test de non-regression de la classe Entity" << endl;
+    assert(position.getX() == 0 && position.getY() == 0 && h == 0 && w == 0);
+    cout << "\tconstructeur par defaut : OK" << endl;
+    Entity e = Entity(Vector2D(1, 2), 3, 4);
+    assert(e.position.getX() == 1 && e.position.getY() == 2 && e.h == 3 && e.w == 4);
+    cout << "\tconstructeur parametre : OK" << endl;
+    assert(e.getPosition.getX() == 1 && e.getPosition.getY() == 2); assert(e.getH() == 3); assert(e.getW == 4);
+    cout << "\taccesseurs : OK" << endl;
+    cout << "Test de non-regression de la classe Entity : OK" << endl;
 }
--- src/core/Entity.h
+++ src/core/Entity.h
@@ -2,24 +2,71 @@
 #define _ENTITY
 
 #include "Vector2D.h"
-/* J'ai volé se code du pacman*/
-class Entity
-{
-private:
-    Vector2D position;
-    unsigned int h;
-    unsigned int w;
 
-public:
-    Entity();
+class Entity {
+    private:
 
-    Entity(Vector2D Eposition , unsigned int Eh, unsigned int Ew);
+        /**
+         * @brief Position de l'entite
+         * 
+         */
+        Vector2D position;
 
-    unsigned int GetH();
+        /**
+         * @brief Hauteur de la hitbox de l'entite
+         * 
+         */
+        unsigned int h;
 
-    unsigned int GetW();
+        /**
+         * @brief Largeur de la hitbox de l'entite
+         * 
+         */
+        unsigned int w;
 
-    Vector2D GetPosition();
+    public:
+
+        /**
+         * @brief Constructeur par defaut de l'entite
+         * 
+         */
+        Entity();
+
+        /**
+         * @brief Constructeur parametre de l'entite
+         * 
+         * @param Eposition 
+         * @param Eh 
+         * @param Ew 
+         */
+        Entity(Vector2D Eposition , unsigned int Eh, unsigned int Ew);
+
+        /**
+         * @brief Accesseur de la hauteur de la hitbox de l'entite
+         * 
+         * @return unsigned int 
+         */
+        unsigned int getH();
+
+        /**
+         * @brief Accesseur de la largeur de la hitbox de l'entite
+         * 
+         * @return unsigned int 
+         */
+        unsigned int getW();
+
+        /**
+         * @brief Accesseur de la position de l'entite
+         * 
+         * @return Vector2D 
+         */
+        Vector2D getPosition();
+
+        /**
+         * @brief Test de non-regression
+         * 
+         */
+        void regressionTest();
 };
 #endif
 
