61a2cc213c67abe3d56c9c330bb62a5aac45cbb9
IvanSonCode <ivanarnaud2@gmail.com>
2023-03-14 09:18:41 +0100

    génération aléatoire de salle

--- src/core/building/Building.cpp
+++ src/core/building/Building.cpp
@@ -1,11 +1,29 @@
 #include "Building.h"
 #include <iostream>
 #include <assert.h>
+#include <stdlib.h> 
+#include <time.h> 
 using namespace std;
 
 Building::Building()
 {
+    int aleatoire;
+    int min = 1, max = 3;
+    int nombreDeValeurs = max - min + 1 ;
+    srand((unsigned int)time(NULL));
     arrayRoom = new Room[NB_ROOM];
+    string f="../../daty/entry.txt";
+    arrayRoom[0]= Room(f);
+    int i;
+    for(i=1;i<NB_ROOM;i++)
+    {
+        aleatoire = (rand() % nombreDeValeurs) + min;
+        f="../../daty/room" + aleatoire ;
+        f= f + ".txt";
+        arrayRoom[i]=f;
+    }
+    f="../../daty/exit.txt";
+    arrayRoom[NB_ROOM]=f;
     currentRoom = 0;
 }
 
