1333643d0847463afd5f62e5183a4e212ae5ceaf
IvanSonCode <ivanarnaud2@gmail.com>
2023-03-21 10:24:53 +0100

    message de fin de partie

--- src/txt/mainTxt.cpp
+++ src/txt/mainTxt.cpp
@@ -1,7 +1,11 @@
 #include "winTxt.h"
 #include "txtGame.h"
 #include "../core/Game.h"
+
 #include <time.h>
+#include <iostream>
+
+using namespace std;
 
 int main ( int argc, char** argv ) {
 	srand(time(NULL));
@@ -9,5 +13,7 @@ int main ( int argc, char** argv ) {
 	Game ga;
 	txtLoop(ga);
     termClear();
+	if(ga.getPlayer()->getHp()==0) cout << "T'es mort, nul !" << endl ;
+	else cout << "Bravo ! T'es pas nul." << endl ;
 	return 0;
 }
