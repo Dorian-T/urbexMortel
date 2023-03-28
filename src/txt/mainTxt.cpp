2a7c792057f2bc253d211213aa3856eb346c9fd5
virginie <thovir@hotmail.fr>
2023-03-28 10:58:43 +0200

    temps qui regresse mais trop vite

--- src/txt/mainTxt.cpp
+++ src/txt/mainTxt.cpp
@@ -32,7 +32,7 @@ int main ( int argc, char** argv ) {
 	}
 	txtLoop(ga);
     termClear();
-	if(ga.getPlayer()->getHp()==0) cout << "T'es mort, nul !" << endl ;
+	if(ga.getPlayer()->getHp()==0 || ga.getBuilding()->getTimetot()==0) cout << "T'es mort, nul !" << endl ;
 	else cout << "Bravo ! T'es pas nul." << endl ;
 	return 0;
 }
