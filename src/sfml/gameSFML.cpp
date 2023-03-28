ba8fc8d5d2ce751a4162ce1ef0ae8dd75941eb4d
Dorian-T <dorian.tonnis@hotmail.fr>
2023-03-28 10:54:02 +0200

    SFML : draw

--- src/sfml/gameSFML.cpp
+++ src/sfml/gameSFML.cpp
@@ -1,9 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include "../core/entity/Vector2D.h"
 #include "../core/Game.h"
 #include "gameSFML.h"
 using namespace sf;
 
-void sfmlLoop(Game & g, RenderWindow & window) {
+void sfmlLoop(Game & game, RenderWindow & window) {
 	while(window.isOpen()) {
         Event event;
         while(window.pollEvent(event)) {
@@ -13,31 +14,93 @@ void sfmlLoop(Game & g, RenderWindow & window) {
 				switch(event.key.code) {
 
 					case Keyboard::Up:
-						g.getPlayer()->up(g.getBuilding());
+						game.getPlayer()->up(game.getBuilding());
 						break;
 
 					case Keyboard::Right:
-						g.getPlayer()->right(g.getBuilding());
+						game.getPlayer()->right(game.getBuilding());
 						break;
 
 					case Keyboard::Down:
-						g.getPlayer()->down(g.getBuilding());
+						game.getPlayer()->down(game.getBuilding());
 						break;
 
 					case Keyboard::Left:
-						g.getPlayer()->left(g.getBuilding());
+						game.getPlayer()->left(game.getBuilding());
 						break;
 
 					default:
 						break;
 				}
         }
-		sfmlDraw(window);
+		sfmlDraw(game, window);
     }
 }
 
-void sfmlDraw(RenderWindow & window) {
+void sfmlDraw(Game & game, RenderWindow & window) {
 	window.clear(Color::Black);
-	
+	unsigned int spriteSize = window.getSize().x / game.getBuilding()->getCurrentRoom()->getDimX();
+	Obstacle o;
+	for(unsigned int x = 0; x < game.getBuilding()->getCurrentRoom()->getDimX(); ++x)
+		for(unsigned int y = 0; y < game.getBuilding()->getCurrentRoom()->getDimY(); ++y) {
+			o = game.getBuilding()->getCurrentRoom()->getObstacle(Vector2D(x,y));
+			switch (o) {
+
+				case nothing:
+					{
+						RectangleShape nothing(Vector2f(spriteSize, spriteSize));
+						nothing.setPosition(x*spriteSize, y*spriteSize);
+						nothing.setFillColor(Color (119, 136, 153));
+						window.draw(nothing);
+					}
+					break;
+
+				case barbedWire:
+					{
+						RectangleShape barbedWire(Vector2f(spriteSize, spriteSize));
+						barbedWire.setPosition(x*spriteSize, y*spriteSize);
+						barbedWire.setFillColor(Color (255, 69, 0));
+						window.draw(barbedWire);
+					}
+					break;
+
+				case block:
+					{
+						RectangleShape block(Vector2f(spriteSize, spriteSize));
+						block.setPosition(x*spriteSize, y*spriteSize);
+						block.setFillColor(Color (128, 128, 128));
+						window.draw(block);
+					}
+					break;
+
+				case door:
+					{
+						RectangleShape door(Vector2f(spriteSize, spriteSize));
+						door.setPosition(x*spriteSize, y*spriteSize);
+						door.setFillColor(Color (160, 82, 45));
+						window.draw(door);
+					}
+					break;
+
+				case ladder:
+					{
+						RectangleShape ladder(Vector2f(spriteSize, spriteSize));
+						ladder.setPosition(x*spriteSize, y*spriteSize);
+						ladder.setFillColor(Color (176, 196, 222));
+						window.draw(ladder);
+					}
+
+				case trapdoor:
+					{
+						RectangleShape trapdoor(Vector2f(spriteSize, spriteSize));
+						trapdoor.setPosition(x*spriteSize, y*spriteSize);
+						trapdoor.setFillColor(Color (210, 105, 30));
+						window.draw(trapdoor);
+					}
+
+				default:
+					break;
+			}
+		}
 	window.display();
 }
