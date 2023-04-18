e4f20e83fcd5370abe5e249ab8ce8f6f5919a4c5
IvanSonCode <ivanarnaud2@gmail.com>
2023-04-18 08:28:42 +0200

    début de la difficulté pour la version sfml

--- src/sfml/mainSFML.cpp
+++ src/sfml/mainSFML.cpp
@@ -3,14 +3,31 @@
 
 #include <time.h>
 #include <stdlib.h>
+#include <iostream>
 
 using namespace sf;
+using namespace std;
 
-
+int chooseDifficulty() {
+	int diff;
+	cout << "Choisissez la difficulté :" << endl;
+	cout << "1. Facile" << endl;
+	cout << "2. Moyen" << endl;
+	cout << "3. Difficile" << endl;
+	cin >> diff;
+	return diff;
+}
 
 int main() {
     srand(time(NULL));
+    unsigned int d = chooseDifficulty();
     Game game;
+    if (d==2)
+		game.getBuilding()->setTimetot(game.getBuilding()->getTimetot()*15);
+	else if (d==3)
+		game.getBuilding()->setTimetot(game.getBuilding()->getTimetot()*10);
+	else 
+		game.getBuilding()->setTimetot(game.getBuilding()->getTimetot()*20);
     GameSFML GSFML(game);
     GSFML.Loop(game);
 }
